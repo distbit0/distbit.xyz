5c2c079c4abe66820f3bc3c92977a4d61dce6ff5
Dist Bit <adp01@pm.me>
2023-07-05 14:42:20 +0200

    add blockquote indent coloring

---
+++ assets/blockquotes.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+    const allBlockquotes = document.querySelectorAll('blockquote');
+    let maxDepth = 0;
+    const topBlockquotes = document.querySelectorAll('.post > blockquote');
+
+    // Function to calculate the maximum depth of blockquote nesting
+    function calculateDepth(blockquote, depth) {
+        if (depth > maxDepth) {
+            maxDepth = depth;
+        }
+        const nestedBlockquotes = blockquote.querySelectorAll(':scope > blockquote');
+        for (let i = 0; i < nestedBlockquotes.length; i++) {
+            calculateDepth(nestedBlockquotes[i], depth + 1);
+        }
+    }
+
+    for (let i = 0; i < allBlockquotes.length; i++) {
+        calculateDepth(allBlockquotes[i], 0);
+    }
+
+    function colorize(blockquote, depth) {
+        if ((maxDepth - depth) % 2 === 0) {
+            blockquote.style.backgroundColor = '#1a1a1a';
+        } else {
+            blockquote.style.backgroundColor = 'black';
+        }
+
+        const nestedBlockquotes = blockquote.querySelectorAll(':scope > blockquote');
+        for (let i = 0; i < nestedBlockquotes.length; i++) {
+            colorize(nestedBlockquotes[i], depth + 1);
+        }
+    }
+
+    for (let i = 0; i < topBlockquotes.length; i++) {
+        colorize(topBlockquotes[i], 0);
+    }
+};
+
